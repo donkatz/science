f5869f5301af9f73126da3e56a791b895a15ad7f
donkatz <82435693+donkatz@users.noreply.github.com>
2021-09-25 15:22:53 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,48 +1,38 @@
 $(document).ready(function () {
 
-                var swiper01 = new Swiper(".mySwiper01", {
-                    spaceBetween: 30,
-                    centeredSlides: true,
-                    autoplay: {
-                        delay: 4000,
-                        disableOnInteraction: false,
-                    },
-                    pagination: {
-                        el: ".swiper-pagination",
-                        clickable: true,
-                    },
-                    navigation: {
-                        nextEl: ".swiper-button-next",
-                        prevEl: ".swiper-button-prev",
-                    },
-                });
+    $('.autoplay > .slick-wrapper').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 
 
-                var swiper02 = new Swiper(".mySwiper02", {
-                    slidesPerView: 5,
-                    loop: true,
-                    loopFillGroupWithBlank: true,
-                    navigation: {
-                        nextEl: ".button-next",
-                        prevEl: ".button-prev",
-                    },
-                });
+    var swiper02 = new Swiper(".mySwiper02", {
+        slidesPerView: 5,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        navigation: {
+            nextEl: ".button-next",
+            prevEl: ".button-prev",
+        },
+    });
 
-                function fun() {
-                    var num = 0;
-                    var result;
-                    setInterval(function () {
-                        result = num * -620;
-                        num++;
-                        console.log(result);
-                        if (num == 3) {
-                            num = 0;
-                        }
-                        $(".slider").stop().animate({
-                            left: result + 'px'
-                        });
-                    }, 3000);
-                }
-                fun();
-
+    function fun() {
+        var num = 0;
+        var result;
+        setInterval(function () {
+            result = num * -620;
+            num++;
+            console.log(result);
+            if (num == 3) {
+                num = 0;
+            }
+            $(".slider").stop().animate({
+                left: result + 'px'
             });
+        }, 3000);
+    }
+    fun();
+
+});
